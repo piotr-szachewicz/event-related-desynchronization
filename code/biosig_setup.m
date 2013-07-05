806d5b92fbe564559fabf95417739f51a95d7695
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2013-07-05 18:57:01 +0200

    Scripts to set up the BIOSig library.

---
+++ code/biosig_setup.m
@@ -0,0 +1,31 @@
+more off;
+
+if !exist('settings.m', 'file')
+    printf('File settings.m does not exist, please create one. Example: settings_example.m');
+    return;
+end
+
+c = settings();
+
+pkg load signal;
+
+printf("====================== SETTING PATHS ======================\n")
+
+addpath(strcat(pwd, '/classifier/'));
+addpath(strcat(pwd, '/classifier/experiments/'));
+addpath(strcat(pwd, '/utils/'));
+addpath(strcat(pwd, '/visualization'));
+addpath(strcat(pwd, '/indiv_bands'));
+addpath(pwd);
+
+% if it already exist, we don't install biosig once again.
+if exist('sload')
+    return
+end
+
+printf("====================== LOADING BIOSIG ======================\n")
+cd(c.BIOSIG_PATH);
+run install.m
+cd(strcat(c.ERDRS_PATH,'code/'));
+printf("============================================================\n")
+
---
+++ code/settings_example.m
@@ -0,0 +1,5 @@
+function const = settings()
+	conts = []
+	const.BIOSIG_PATH='~/programs/biosig-svn/biosig';
+	const.ERDRS_PATH='~/repositories/event-related-desynchronization/'
+end
